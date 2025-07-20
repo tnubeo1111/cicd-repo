2228ac123d4ef491916bb3f5a03d0870ff8ed5f6
tnubeo1111 <pelu0377583903@gmail.com>
2025-07-20 19:59:40 +0700

    edit(): application

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -12,8 +12,9 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
+        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven - ThaLT");
+        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD - Le Thanh Tha");
+    
         return "index";
     }
 
