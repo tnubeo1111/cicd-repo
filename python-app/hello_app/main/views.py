e82064655c6bc1f10f036db640b3611d74855018
tnubeo1111 <pelu0377583903@gmail.com>
2025-07-28 20:14:23 +0700

    add(): python-app

---
+++ python-app/hello_app/main/views.py
@@ -0,0 +1,14 @@
+import logging
+from django.shortcuts import render
+from django.http import HttpResponse
+
+logger = logging.getLogger('django.request')
+
+# Create your views here.
+def hello(request):
+    response = HttpResponse("Hello App")
+    logger.info(
+        f"Request from {request.META.get('REMOTE_ADDR')} - "
+        f"Path: {request.path} - Status: {response.status_code}"
+    )
+    return response
