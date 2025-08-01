27c181e509bb0818d1716b26f23deeaf152fb29a
tnubeo1111 <pelu0377583903@gmail.com>
2025-08-01 20:16:49 +0700

    modify(): file

--- python-app/hello_app/main/views.py
+++ python-app/hello_app/main/views.py
@@ -2,12 +2,14 @@ import logging
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 logger = logging.getLogger('django.request')
 
-def home(request):
+# Create your views here.
+def hello(request):
     response = HttpResponse("Hello App")
-    ip = request.META.get('REMOTE_ADDR', 'Unknown IP')
     logger.info(
-        f"Request from {ip} - Path: {request.path} - Status: {response.status_code}"
+        f"Request from {request.META.get('REMOTE_ADDR')} - "
+        f"Path: {request.path} - Status: {response.status_code}"
     )
     return response
