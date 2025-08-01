0a372f1e234e2ca07858ff32bd25aba21d0811c0
tnubeo1111 <pelu0377583903@gmail.com>
2025-08-01 19:49:55 +0700

    modify(): file

--- python-app/hello_app/main/views.py
+++ python-app/hello_app/main/views.py
@@ -1,14 +1,22 @@
-import logging
-from django.shortcuts import render
 from django.http import HttpResponse
+import logging
 
+# Setting up a logger for the application
 logger = logging.getLogger('django.request')
 
-# Create your views here.
-def hello(request):
-    response = HttpResponse("Hello App")
-    logger.info(
-        f"Request from {request.META.get('REMOTE_ADDR')} - "
-        f"Path: {request.path} - Status: {response.status_code}"
-    )
-    return response
+# View for the home page
+def home(request):
+    logger.info(f'Home Access - From: {request.META.get("REMOTE_ADDR")}')
+    return HttpResponse('<h1>Welcome to DevOps Simple Web!</h1>')
+
+
+def about(request):
+    return HttpResponse('<h2>About: Đây là web mẫu cho DevOps!</h2>')
+
+def jobs(request):
+    items = ['CI/CD Pipeline', 'Docker', 'Kubernetes', 'Monitoring', 'Logging']
+    html = "<h3>DevOps Tasks:</h3><ul>"
+    for item in items:
+        html += f"<li>{item}</li>"
+    html += "</ul>"
+    return HttpResponse(html)
