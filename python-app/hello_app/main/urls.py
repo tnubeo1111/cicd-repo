bdbf8f1d8394971b52b3e3222788328ec5021fc7
tnubeo1111 <pelu0377583903@gmail.com>
2025-08-01 20:11:16 +0700

    modify(): file

--- python-app/hello_app/main/urls.py
+++ python-app/hello_app/main/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('jobs/', views.jobs, name='jobs'),
+    path('', views.hello, name='home'),
 ]
--- python-app/hello_app/main/views.py
+++ python-app/hello_app/main/views.py
@@ -1,19 +1,13 @@
-from django.http import HttpResponse
 import logging
+from django.shortcuts import render
+from django.http import HttpResponse
 
-# Setting up a logger for the application
 logger = logging.getLogger('django.request')
 
-# View for the home page
 def home(request):
-    logger.info(f'Home Access - From: {request.META.get("REMOTE_ADDR")}')
-    return HttpResponse('<h1>Welcome to DevOps Simple Web!</h1>')
-
-def jobs(request):
-    logger.info(f'Jobs Page Access - From: {request.META.get("REMOTE_ADDR")}')
-    items = ['CI/CD Pipeline', 'Docker', 'Kubernetes', 'Monitoring', 'Logging']
-    html = "<h3>DevOps Tasks:</h3><ul>"
-    for item in items:
-        html += f"<li>{item}</li>"
-    html += "</ul>"
-    return HttpResponse(html)
+    response = HttpResponse("Hello App")
+    ip = request.META.get('REMOTE_ADDR', 'Unknown IP')
+    logger.info(
+        f"Request from {ip} - Path: {request.path} - Status: {response.status_code}"
+    )
+    return response
