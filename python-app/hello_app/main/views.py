83922aa3a806d1d4bea033d9cf41aa98c97539d8
tnubeo1111 <pelu0377583903@gmail.com>
2025-08-01 20:19:52 +0700

    modify(): file

--- python-app/hello_app/main/views.py
+++ python-app/hello_app/main/views.py
@@ -6,10 +6,14 @@ from django.http import HttpResponse
 logger = logging.getLogger('django.request')
 
 # Create your views here.
-def hello(request):
+def home(request):
     response = HttpResponse("Hello App")
     logger.info(
         f"Request from {request.META.get('REMOTE_ADDR')} - "
         f"Path: {request.path} - Status: {response.status_code}"
     )
     return response
+
+# Create a view tools for DevOps
+# def devops_tools(request):
+#     items = ['CICD', 'Monitoring', 'Logging', 'Scaling']
