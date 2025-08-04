dfe8f9b4a91597d0c3b88ae30ca758396e59c875
tnubeo1111 <pelu0377583903@gmail.com>
2025-08-04 18:34:04 +0700

    modify(): file

--- python-app/hello_app/main/views.py
+++ python-app/hello_app/main/views.py
@@ -1,10 +1,13 @@
 import logging
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.http import require_GET
+
 
 logger = logging.getLogger('django.request')
 
 # Create your views here.
+@require_GET
 def hello(request):
     response = HttpResponse("Hello App")
     logger.info(
